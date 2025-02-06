2184ced74d5d7e46bf5b416636f45913816c52c1
haruki-nikaidou <nharuki@tuta.com>
2025-02-06 19:05:08 +0900

    update some style

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,6 +11,7 @@ const blog = defineCollection({
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
 		pinned: z.boolean().optional(),
+		tags: z.array(z.string()).optional(),
 		notCompleted: z.boolean().optional(),
 	}),
 });
