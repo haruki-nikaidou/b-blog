c6c58706d64ee5c63336be5927b7667ef494c12c
Nikaidou Haruki <nharuki@pm.me>
2025-08-21 18:05:56 +0900

    change the name

--- src/consts.ts
+++ src/consts.ts
@@ -1,19 +1,19 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Haruki\'s blog';
+export const SITE_TITLE = 'Eureka\'s blog';
 
-export const SITE_DESCRIPTION = 'Haruki\'s blog';
+export const SITE_DESCRIPTION = 'Eureka\'s blog';
 
 export const FAVICON_SRC = '/favicon.svg';
 
 export const AVATAR_SRC = '/avatar.svg';
 
-export const BIO = "願ふこと、星に託せど。届かぬ夢に、石こそ返る。";
+export const BIO = "星に祈れど、空は答えず、堕ちるは常に、隕の石のみ。";
 
 export const GLOBAL_STYLE: 'quartz' | 'glass' | 'lightGlass' = 'lightGlass'
 
-export const COPYRIGHT_NAME = 'Nikaidou Haruki';
+export const COPYRIGHT_NAME = 'Sakurakoji Eureka';
 
 export function imageDeliveryUrl(id: string, variant: 'public' | 'small'): string {
   return `https://imagedelivery.net/6gszw1iux5BH0bnwjXECTQ/${id}/${variant}`
