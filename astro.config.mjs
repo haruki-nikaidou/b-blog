5763d0f9a82b058cca76ca1f5bd76254693bf035
Nikaidou Haruki <nharuki@pm.me>
2025-10-05 02:42:38 +0900

    add support for mermaid

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,21 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
+import rehypeMermaid from 'rehype-mermaid';
 
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
+    syntaxHighlight: {
+      type: 'shiki',
+      excludeLangs: ['mermaid', 'math'],
+    },
     remarkPlugins: [remarkMath],
     rehypePlugins: [[rehypeKatex, {
       // Katex plugin options
-    }]]
+    }], rehypeMermaid]
   },
 
   image: {
