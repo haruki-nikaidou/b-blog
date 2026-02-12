29a89f73a69253b031fe5bf17bc1cf106d629915
Nikaidou Haruki <nharuki@pm.me>
2026-02-12 22:08:32 +0900

    add hero image origin info

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,14 +7,24 @@ const blog = defineCollection({
 		base: 'content/blog',
 	}),
 	schema: z.object({
+		// Metadata
 		title: z.string(),
 		description: z.string(),
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
+		draft: z.boolean().optional(),
+
+		// Hero Image
 		heroImageId: z.string().optional(),
+		heroImageSource: z.string().optional(),
+		heroImageSourceUrl: z.string().optional(),
+		heroImageAuthor: z.string().optional(),
+		heroImageAuthorUrl: z.string().optional(),
+		heroImageOwned: z.boolean().default(false),
+
+		// content metadata
 		pinned: z.boolean().optional(),
 		tags: z.array(z.string()),
-		draft: z.boolean().optional(),
 	}),
 });
 
